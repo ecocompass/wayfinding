83ae296adfbba980536ca77c115a95f247c9fbdd
Sahil Naikdhure <sanaikdhure@gmail.com>
2024-02-06 17:24:02 +0000

    add constant line

--- App.tsx
+++ App.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -5,8 +8,8 @@
  * @format
  */
 
-import React, {useEffect, useState} from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect, useState } from "react";
+import type { PropsWithChildren } from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -19,9 +22,9 @@ import {
   Button,
   PermissionsAndroid,
   Platform,
-} from 'react-native';
+} from "react-native";
 
-import Geolocation from 'react-native-geolocation-service';
+import Geolocation from "react-native-geolocation-service";
 
 import {
   Colors,
@@ -29,133 +32,49 @@ import {
   Header,
   LearnMoreLinks,
   ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+} from "react-native/Libraries/NewAppScreen";
 
-import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
-import { RootSiblingParent } from 'react-native-root-siblings';
-import Toast from 'react-native-root-toast';
-import Mapbox from '@rnmapbox/maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import { RootSiblingParent } from "react-native-root-siblings";
+import Toast from "react-native-root-toast";
+import Mapbox from "@rnmapbox/maps";
+import { LineLayer, ShapeSource } from "@rnmapbox/maps";
 
-Mapbox.setAccessToken('pk.eyJ1IjoiYWdyYXdhc2EiLCJhIjoiY2xyeXdtbGgyMHVhMTJxbnV4dWQyeXFiNCJ9.EBZDzXD0wMCa6hQNAn-4Xg');
-
+Mapbox.setAccessToken(
+  "pk.eyJ1IjoiZWxlY3Rybzc1IiwiYSI6ImNscnRlcWJ1eDAxN2QycW82cXp5MWZsbXMifQ.ZlRWWO347Yae46luSV8BCA"
+);
 
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   container: {
     height: 900,
     width: 720,
   },
   map: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
 
-// function Section({children, title}: SectionProps): JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// }
-
-// function App(): JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   const [isLoading, setLoading] = useState(true);
-//   const [data, setData] = useState<Movie[]>([]);
-
-//   // get data at app start
-//   const getMovies = async () => {
-//     try {
-//       const response = await fetch('https://reactnative.dev/movies.json');
-//       const json = await response.json();
-//       setData(json.movies);
-//     } catch (error) {
-//       console.error(error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   useEffect(() => {
-//     getMovies();
-//   }, []);
-
-//   if (isLoading) {
-//     return (
-//       <ScrollView>
-//         <Section title="Getting Movies">Loading...</Section>
-//       </ScrollView>
-//     );
-//   } else {
-//     const { width, height } = Dimensions.get("window");
-//     return (
-//       <ScrollView>
-//         <Section title="Sahil">{data[0].title}</Section>
-//         <MapView
-//           provider={PROVIDER_GOOGLE}
-//           initialRegion={{
-//             latitude: 53.3498,
-//             longitude: -6.2603,
-//             latitudeDelta: 0.0922,
-//             longitudeDelta: 0.0421,
-//           }}
-//           style={{
-//             width,
-//             height,
-//           }}
-//         />
-//       </ScrollView>
-//     );
-//   }
-// }
-// function to check permissions and get Location
-
-// Function to get permission for location
+// Function to get permission for location from system
 const requestLocationPermission = async () => {
   try {
     const granted = await PermissionsAndroid.request(
       PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
       {
-        title: 'Geolocation Permission',
-        message: 'Can we access your location?',
-        buttonNeutral: 'Ask Me Later',
-        buttonNegative: 'Cancel',
-        buttonPositive: 'OK',
-      },
+        title: "Geolocation Permission",
+        message: "Can we access your location?",
+        buttonNeutral: "Ask Me Later",
+        buttonNegative: "Cancel",
+        buttonPositive: "OK",
+      }
     );
-    if (granted === 'granted') {
-      console.log('You can use Geolocation');
+    if (granted === "granted") {
       return true;
     } else {
-      console.log('You cannot use Geolocation');
       return false;
     }
   } catch (err) {
@@ -164,32 +83,37 @@ const requestLocationPermission = async () => {
 };
 
 const App = () => {
-  const [location, setLocation] = useState({latitude: 0, longitude: 0});
   const [currentLocation, setCurrentLocation] = useState([0, 0]); // Longitude, Latitude
 
+  let startingPoint = [-6.2653554, 53.324153];
+  let destinationPoint = [-6.2650513, 53.3256942];
+
   useEffect(() => {
-    if (Platform.OS === 'android') {
-      PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-      ).then(granted => {
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          getCurrentLocation();
+    if (Platform.OS === "android") {
+      const result = requestLocationPermission().then((res) => {
+        if (res) {
+          Geolocation.getCurrentPosition(
+            (position) => {
+              setCurrentLocation([
+                position.coords.longitude,
+                position.coords.latitude,
+              ]);
+            },
+            (error) => {
+              console.log(error.code, error.message);
+              setDefaultLocation();
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+          );
         }
       });
     } else {
-      getCurrentLocation();
+      setDefaultLocation();
     }
   }, []);
 
-  const getCurrentLocation = () => {
-    Geolocation.getCurrentPosition(
-      position => {
-        setCurrentLocation([position.coords.longitude, position.coords.latitude]);
-        console.log("Pos:",position)
-      },
-      error => console.log(error),
-      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
+  const setDefaultLocation = () => {
+    setCurrentLocation([0, 0]);
   };
 
   const getLocationData = async (data: any) => {
@@ -197,124 +121,115 @@ const App = () => {
       // const response = await fetch('https://reactnative.dev/movies.json');
 
       const response = await fetch(
-        `http://ecocompass.anupal.me/test-core?latitude=${encodeURIComponent(data.latitude)}&longitude=${encodeURIComponent(data.longitude)}`, { method: "GET"})
+        `http://ecocompass.anupal.me/test-core?latitude=${encodeURIComponent(
+          data.latitude
+        )}&longitude=${encodeURIComponent(data.longitude)}`,
+        { method: "GET" }
+      );
 
       const json = await response.json();
       Toast.show(json.message, {
-        duration: 10000
-      })
-      console.log(json)
+        duration: 10000,
+      });
+      console.log(json);
     } catch (error) {
       // console.error(error);
     } finally {
       // setLoading(false);
-
     }
   };
-  const getLocation = () => {
-    const result = requestLocationPermission();
-    result.then(res => {
-      if (res) {
-        Geolocation.getCurrentPosition(
-          position => {
-            setLocation({
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude,
-            });
-            console.log("Pos2",position);
-            console.log(currentLocation)
-            
-        //    getLocationData({latitude: position.coords.latitude, longitude: position.coords.longitude});
-          },
-          error => {
-            // See error code charts below.
-            console.log(error.code, error.message);
-            // setLocation(false);
-          },
-          {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-        );
-      }
+
+  const getUserClickLoc = function (loc) {
+    let locObj = loc.nativeEvent.coordinate;
+    getLocationData({
+      latitude: locObj.latitude,
+      longitude: locObj.longitude,
     });
   };
-  if (!location.latitude) {
-    return (
-      <RootSiblingParent>
+
+  const route: any = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        properties: {},
+        geometry: {
+          type: "LineString",
+          coordinates: [
+            [-6.2653554, 53.324153],
+            [-6.2653123, 53.3243724],
+            [-6.2651821, 53.325035],
+            [-6.2651734, 53.3250898],
+            [-6.2651149, 53.3253791],
+            [-6.2650513, 53.3256942],
+          ],
+        },
+      },
+    ],
+  };
+
+  // const route: any = [
+
+  // ];
+
+  return (
+    <RootSiblingParent>
+      <View style={styles.page}>
         <View style={styles.container}>
-          <Text>Welcome!</Text>
-          <View
-            style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
-            <Button title="Get Location" onPress={getLocation} />
-          </View>
+          <Mapbox.MapView style={styles.map}>
+            <Mapbox.Camera
+              zoomLevel={18}
+              centerCoordinate={startingPoint}
+              animationMode={"flyTo"}
+              animationDuration={2000}
+            />
+            <Mapbox.PointAnnotation
+              id="startingPoint"
+              coordinate={startingPoint}
+            >
+              <View
+                style={{
+                  height: 20,
+                  width: 20,
+                  backgroundColor: "#00cccc",
+                  borderRadius: 15,
+                  borderColor: "#fff",
+                  borderWidth: 1,
+                }}
+              />
+            </Mapbox.PointAnnotation>
+            <Mapbox.PointAnnotation
+              id="destinationPointAnnotation"
+              coordinate={destinationPoint}
+            >
+              <View
+                style={{
+                  height: 20,
+                  width: 20,
+                  backgroundColor: "#00cccc",
+                  borderRadius: 15,
+                  borderColor: "#fff",
+                  borderWidth: 1,
+                }}
+              />
+            </Mapbox.PointAnnotation>
+            {route && (
+              <Mapbox.ShapeSource id="shapeSource" shape={route}>
+                <Mapbox.LineLayer
+                  id="lineLayer"
+                  style={{
+                    lineWidth: 3,
+                    lineJoin: "bevel",
+                    lineColor: "#0000ff",
+                  }}
+                />
+              </Mapbox.ShapeSource>
+            )}
+          </Mapbox.MapView>
         </View>
-      </RootSiblingParent>
-    );
-  } else {
-    const { width, height } = Dimensions.get("window");
-    const userRegion = {
-      latitude: location.latitude,
-      longitude: location.longitude,
-      latitudeDelta: 90.0922,
-      longitudeDelta: 66.0421,
-    };
-
-    const getUserClickLoc = function(loc) {
-      let locObj = loc.nativeEvent.coordinate;
-      getLocationData({latitude: locObj.latitude, longitude: locObj.longitude});
-    }
-
-    return (
-      <RootSiblingParent>
-        
-        {/*   <MapView
-            provider={PROVIDER_GOOGLE}
-            initialRegion={userRegion}
-            style={{
-              width,
-              height,
-            }}
-            onPress={getUserClickLoc}>
-            <Marker coordinate={userRegion} />
-          </MapView> */}
-          <View style={styles.page}>
-           <View style={styles.container}>
-        
-            {/* <Marker coordinate={userRegion} /> */}
-              <Mapbox.MapView style={styles.map}>
-              <Mapbox.Camera
-          zoomLevel={10}
-          centerCoordinate={currentLocation}
-          animationMode={'flyTo'}
-          animationDuration={2000}
-         // centerCoordinate={[-74.0060, 40.7128]} // Long, Lat of New York City
-        />
-        <Mapbox.PointAnnotation
-           id="currentLocation"
-           coordinate={currentLocation}
-        >
-          <View style={{
-            height: 30,
-            width: 30,
-            backgroundColor: '#00ff00',
-            borderRadius: 15,
-            borderColor: '#fff',
-            borderWidth: 3,
-          }} />
-        </Mapbox.PointAnnotation>
-      </Mapbox.MapView>
-            </View>
-            </View>
-      </RootSiblingParent>
-    );
-  }
+      </View>
+    </RootSiblingParent>
+  );
 };
 
-/* const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-*/
-export default App; 
+export default App;
