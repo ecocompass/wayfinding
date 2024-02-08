876e8f677d75b565c549dee4b8dc907e9b417b32
Sahil Naikdhure <sanaikdhure@gmail.com>
2024-02-08 15:48:41 +0000

    add ui framework gluestack

--- App.tsx
+++ App.tsx
@@ -4,25 +4,29 @@ import Map from "./components/Map/map";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Auth from "./components/Auth/login";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config";
 
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <RootSiblingParent>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Auth"
-            component={Auth}
-            /*  options={{ title: 'Welcome' }} */
-          />
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Map"
-            component={Map}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <GluestackUIProvider config={config}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Auth"
+              component={Auth}
+              /*  options={{ title: 'Welcome' }} */
+            />
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="Map"
+              component={Map}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </GluestackUIProvider>
     </RootSiblingParent>
   );
 };
