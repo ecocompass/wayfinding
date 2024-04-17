9119f34739bfa6fe37859370af12b20345eaff15
Samarth Agrawal <agrawal.samarth123@gmail.com>
2024-04-17 17:18:55 +0100

    updated test case

--- __tests__/map.test.tsx
+++ __tests__/map.test.tsx
@@ -1,4 +1,4 @@
-// AuthComponent.test.tsx
+
 import React from "react";
 import { render } from "@testing-library/react-native";
 import Map from "../components/Map/map";
@@ -6,18 +6,25 @@ import { Provider } from "react-redux";
 import configureMockStore from 'redux-mock-store';
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
+import Profile from "../components/Settings/profile";
 
 jest.mock('@rnmapbox/maps');
 jest.mock('react-native-geolocation-service');
 let mockStore = configureMockStore();
-const store = mockStore([])
+const store = mockStore({
+    userDetails:{
+        profile:{
+            first_name:'',
+            last_name:''
+        }
+    }
+})
 describe("MapComponent", () => {
 
-  it('should render search box correctly', () => {
-    const { getByTestId,getByPlaceholderText } = render(<Provider store={store}>   <GluestackUIProvider config={config}><Map />   </GluestackUIProvider></Provider>);
-    expect(getByTestId('map-view')).toBeTruthy();
-    //expect(getByPlaceholderText('Take me  somewhere')).toBeTruthy();
-  });
+    it('should render search box correctly', () => {
+        const { getByText } = render(<Provider store={store}>   <GluestackUIProvider config={config}><Profile />   </GluestackUIProvider></Provider>);
+        expect(getByText('+353 213193212')).toBeTruthy();
+    });
 
 
 });
